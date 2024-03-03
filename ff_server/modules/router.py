a527a7b999f60a37b2485755a386d0ee6c0ab8ba
nnnLik <rslmakhmudov@gmail.com>
2024-03-03 12:46:44 +0300

    core to router

--- ff_server/modules/router.py
+++ ff_server/modules/router.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from .communication.controllers import router as communication_router
+from .core.controller import router as core_router
+
 
 router = APIRouter(prefix="/v1")
 
-router.include_router(
-    communication_router, prefix="/communication", tags=["communication"]
-)
+router.include_router(communication_router, prefix="/communication", tags=["communication"])
+router.include_router(core_router, prefix="/core", tags=["core", "system"])
+
